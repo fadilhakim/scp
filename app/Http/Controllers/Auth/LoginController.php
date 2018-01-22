0abeeca737a6370ae5eace2a729ad078f69d9d27
Kunglaw <alhusna901@gmail.com>
2018-01-22 21:35:47 +0700

    backup all Controller,  model, and view

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,23 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
+use Validator;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+
+use Cartalyst\Sentinel\Checkpoints\NotActivatedException;
+use Cartalyst\Sentinel\Checkpoints\ThrottlingException;
+use Sentinel;
+use Illuminate\Http\Request;
+use Activation;
+use Redirect;
+use Session;
+use Illuminate\Support\Facades\Input;
+use Mail;
+use Carbon\Carbon;
+use Mailchimp;
+use App\ZipCode;
 
 class LoginController extends Controller
 {
@@ -18,14 +33,14 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+   use  ThrottlesLogins;
 
     /**
      * Where to redirect users after login.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboard';
 
     /**
      * Create a new controller instance.
@@ -36,4 +51,63 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+    
+    protected function login(Request $request)
+    {
+
+
+        try {
+
+            // Validation
+            $validation = Validator::make($request->all(), [
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            if ($validation->fails()) {
+                return Redirect::back()->withErrors($validation)->withInput();
+            }
+            $remember = (Input::get('remember') == 'on') ? true : false;
+            if ($user = Sentinel::authenticate($request->all(), $remember)) {
+                
+                   return redirect('dashboard'); 
+                
+            }
+
+            return Redirect::back()->withErrors(['global' => 'Invalid password or this user does not exist' ]);
+
+        } catch (NotActivatedException $e) {
+            return Redirect::back()->withErrors(['global' => 'This user is not activated','activate_contact'=>1]);
+
+        }
+        catch (ThrottlingException $e) {
+            $delay = $e->getDelay();
+            return Redirect::back()->withErrors(['global' => 'You are temporary susspended' .' '. $delay .' seconds','activate_contact'=>1]);
+        }
+
+        return Redirect::back()->withErrors(['global' => 'Login problem please contact the administrator']);
+
+        
+    }
+    
+
+    protected function logout()
+    {
+        Sentinel::logout();
+        return redirect('/');
+    }
+    protected function activate($id){
+        $user = Sentinel::findById($id);
+
+        $activation = Activation::create($user);
+        $activation = Activation::complete($user, $activation->code);
+        Session::flash('message', trans('messages.activation'));
+        Session::flash('status', 'success');
+        return redirect('login');
+    }
+
 }
---
+++ database/migrations/backup_migration/2018_01_17_044520_create_sliders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('image_name',100);
+            $table->string('url_image',200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sliders');
+    }
+}
