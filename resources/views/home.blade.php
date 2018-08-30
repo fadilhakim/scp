30fe7e1bdeb8b63ecb11196acfa85f200addc8b1
fadilhakim <fadil.nylon@gmail.com>
2018-08-30 16:19:16 +0800

    home 3baner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,23 +11,16 @@
             <div class="swiper-container" data-parallax="1" data-auto-height="1">
                <div class="swiper-wrapper">
 
-                <div class="swiper-slide" style="background-image: url('{{ URL::asset('public/sliders/banana_slider.jpg')}}'); height: 450px;">
+                <?php $slider = App\Models\Slider::all_slider(); 
+                        foreach($slider as $rowSlider) {
+                ?>
+                    <div class="swiper-slide" style="background-image: url('{{ URL::asset('public/sliders/'.$rowSlider->image_name)}}'); height: 450px;">
                         <div class="container">
                            
                         </div>
                    </div>
+                <?php } ?>
 
-                   <div class="swiper-slide" style="background-image: url('{{ URL::asset('public/sliders/army_slider.jpg')}}'); height: 450px;">
-                        <div class="container">
-                           
-                        </div>
-                   </div>
-                   
-                   <div class="swiper-slide" style="background-image: url('{{ URL::asset('public/sliders/army_slider.jpg')}}'); height: 450px;">
-                        <div class="container">
-                           
-                        </div>
-                   </div>
                </div>
                <div class="swiper-pagination swiper-pagination-white"></div>
             </div>
@@ -37,44 +30,44 @@
             <div class="empty-space col-xs-b30"></div>
             <div class="container">
                 <div class="row">
-                    <div class="col-md-6 col-xs-b30 col-md-b0">
-                        <div class="banner-shortcode style-3 rounded-image text-center" style="background-image: url(img/background-9.jpg);">
-                            <div class="valign-middle-cell">
+                    <div class="col-md-4">
+                        <div class="banner-shortcode style-5 rounded-image" style="background-image: url({{ URL::asset('public/img/background-16.jpg')}});)">
+                            <div class="valign-middle">
                                 <div class="valign-middle-content">
-                                    <div class="simple-article size-3 light transparent uppercase col-xs-b5">relax collection</div>
-                                    <h3 class="h3 light"><a href="#">your perfect sound</a></h3>
-                                    <div class="title-underline center"><span></span></div>
-                                    <div class="simple-article size-3 light transparent col-xs-b30">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesentir pulvinar ante et nisl scelerisque. Mauris dignissim metus mollis neque rhoncus in nunc sit amet </div>
-                                    <a class="button size-2 style-1" href="#">
-                                        <span class="button-wrapper">
-                                            <span class="icon"><img src="img/icon-1.png" alt=""></span>
-                                            <span class="text">learn more</span>
-                                        </span>
-                                    </a>
+                                    <div class="simple-article size-3 light transparent uppercase col-xs-b5">street collection</div>
+                                    <h4 class="h4 light col-xs-b5"><a href="#">noise is not a problem</a></h4>
+                                    <div class="simple-article size-5 light transparent uppercase">starting from <span class="color">$95.00</span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="banner-shortcode style-5 rounded-image" style="background-image: url({{ URL::asset('public/img/background-16.jpg')}});)">
+                            <div class="valign-middle">
+                                <div class="valign-middle-content">
+                                    <div class="simple-article size-3 light transparent uppercase col-xs-b5">street collection</div>
+                                    <h4 class="h4 light col-xs-b5"><a href="#">noise is not a problem</a></h4>
+                                    <div class="simple-article size-5 light transparent uppercase">starting from <span class="color">$95.00</span></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="banner-shortcode style-5 rounded-image" style="background-image: url({{ URL::asset('public/img/background-16.jpg')}});)">
+                            <div class="valign-middle">
+                                <div class="valign-middle-content">
+                                    <div class="simple-article size-3 light transparent uppercase col-xs-b5">street collection</div>
+                                    <h4 class="h4 light col-xs-b5"><a href="#">noise is not a problem</a></h4>
+                                    <div class="simple-article size-5 light transparent uppercase">starting from <span class="color">$95.00</span></div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="empty-space col-xs-b30"></div>
         </div>
 
-        <div class="slider-wrapper">
-            <div class="swiper-button-prev hidden"></div>
-            <div class="swiper-button-next hidden"></div>
-            <div class="swiper-container page-height" data-direction="vertical" data-mousewheel="1">
-                <div class="swiper-wrapper">
-                    <?php $slider = App\Models\Slider::all_slider(); 
-                        foreach($slider as $rowSlider) {
-                    ?>
-                        <div class="swiper-slide">
-                            <img src="{{ URL::asset('public/sliders/'.$rowSlider->image_name)}}" alt="">
-                        </div>
-                    <?php } ?>
-               </div>
-               <div class="swiper-pagination"></div>
-            </div>
-        </div>
     </div> 
 @include('template/footer')
 
